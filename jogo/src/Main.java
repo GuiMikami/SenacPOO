9d1278bc16bb8f51f6732fe498bd945d17af1e2b
Guilherme Mikami <guilherme.mikami@hotmail.com>
2023-02-25 14:51:19 -0300

    jogo

---
+++ jogo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        var jogador = new Jogador("Mikami");
+        System.out.println(jogador.getNome());
+        System.out.println(jogador.getHp());
+    }
+}
